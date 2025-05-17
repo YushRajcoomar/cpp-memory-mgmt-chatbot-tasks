960337f2dc2d6b9b7aeca7da9eca8664d0e9bd62
Yush Rajcoomar <yush2398@live.com>
2025-05-17 19:29:49 -0400

    debug task done

---
+++ src/CMakeFiles/membot.dir/cmake_clean.cmake
@@ -0,0 +1,19 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/membot.dir/chatbot.cpp.o"
+  "CMakeFiles/membot.dir/chatbot.cpp.o.d"
+  "CMakeFiles/membot.dir/chatgui.cpp.o"
+  "CMakeFiles/membot.dir/chatgui.cpp.o.d"
+  "CMakeFiles/membot.dir/chatlogic.cpp.o"
+  "CMakeFiles/membot.dir/chatlogic.cpp.o.d"
+  "CMakeFiles/membot.dir/graphedge.cpp.o"
+  "CMakeFiles/membot.dir/graphedge.cpp.o.d"
+  "CMakeFiles/membot.dir/graphnode.cpp.o"
+  "CMakeFiles/membot.dir/graphnode.cpp.o.d"
+  "membot"
+  "membot.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/membot.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
